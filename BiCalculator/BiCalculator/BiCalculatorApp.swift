772e7301203d277cee2b25e7576d8a8c8340037c
Jay <coooooooderjay@gmail.com>
2023-11-27 12:14:29 +0800

    [chore] - Creation of xcode project & preparing assets

---
+++ BiCalculator/BiCalculator/BiCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  BiCalculatorApp.swift
+//  BiCalculator
+//
+//  Created by jay on 2023/11/25.
+//
+
+import SwiftUI
+
+@main
+struct BiCalculatorApp: App {
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
+    }
+  }
+}
---
+++ BiCalculator/BiCalculator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BiCalculator
+//
+//  Created by jay on 2023/11/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+  var body: some View {
+    VStack {
+      Image(systemName: "globe")
+        .imageScale(.large)
+        .foregroundStyle(.tint)
+      Text("Hello, world!")
+    }
+    .padding()
+  }
+}
+
+#Preview {
+  ContentView()
+}
